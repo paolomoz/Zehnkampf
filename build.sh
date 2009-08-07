87a04cfd924aa6c024afab1da7167885f9b38e2c
Paolo Mottadelli <paolo.moz@gmail.com>
2009-08-07 10:26:56 +0200

    brief review in license

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# Copyright 2009 Paolo Mottadelli <paolo.moz@gmail.com>
+# Copyright 2009 Paolo Mottadelli  <paolo.moz@gmail.com>
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
